554045d562d65af91443dd14717054fa7d39542f
GabrielDSM0 <gabrielcamargonorberto@outlook.com>
2023-12-12 08:37:54 -0300

    avatp2-Gabriel

---
+++ HelperStockBeta/HelperStockBeta.API/Controllers/ProductsController.cs
@@ -0,0 +1,89 @@
+﻿using HelperStockBeta.Application.DTOs;
+using HelperStockBeta.Application.Interfaces;
+using HelperStockBeta.Application.Services;
+using HelperStockBeta.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelperStockBeta.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var product = await _productService.GetProducts();
+            if (product == null)
+            {
+                return NotFound("Produto not found");
+            }
+
+            return Ok(product);
+        }
+
+        [HttpGet("{id:int}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(int id)
+        {
+            var product = await _productService.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound("Product not found");
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO productDto)
+        {
+            if (productDto == null)
+            {
+                return BadRequest("Invalid Body Data");
+            }
+
+            await _productService.Add(productDto);
+
+            return new CreatedAtRouteResult("GetProduct", new { id = productDto.Id }, productDto);
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put(int id, [FromBody] ProductDTO productDto)
+        {
+            if (id != productDto.Id)
+            {
+                return BadRequest("Id not verificated");
+            }
+
+            if (productDto == null)
+            {
+                return BadRequest("DTO inspec fail");
+            }
+
+            await _productService.Update(productDto);
+
+            return Ok(productDto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<ProductDTO>> Delete(int id)
+        {
+            var product = _productService.GetById(id);
+            if (product == null)
+            {
+                return NotFound("The product delete not found");
+            }
+
+            await _productService.Remove(id);
+            return Ok(product);
+        }
+    }
+}
